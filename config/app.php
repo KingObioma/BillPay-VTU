817a178ec405853fd49faca8d9682a188441f9a5
king <kelvinozak0@gmail.com>
2026-01-03 21:20:06 +0100

    Remove bundled stripe library from repo

--- config/app.php
+++ config/app.php
@@ -150,7 +150,6 @@ return [
 		Illuminate\Foundation\Providers\FoundationServiceProvider::class,
 		Illuminate\Hashing\HashServiceProvider::class,
 		Illuminate\Mail\MailServiceProvider::class,
-		Illuminate\Mail\MailServiceProvider::class,
 		Illuminate\Notifications\NotificationServiceProvider::class,
 		Illuminate\Pagination\PaginationServiceProvider::class,
 		Illuminate\Pipeline\PipelineServiceProvider::class,
