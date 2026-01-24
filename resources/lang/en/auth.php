bc164479055bd3bd5ee65f215a821e559efaf835
king <kelvinozak0@gmail.com>
2026-01-24 22:20:54 +0100

    Now Laravel 12 upgrade

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'failed' => 'These credentials do not match our records.',
-    'incorrect' => 'These credentials are absolutely wrong.',
+    'incorrect' => 'These details are absolutely wrong.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
