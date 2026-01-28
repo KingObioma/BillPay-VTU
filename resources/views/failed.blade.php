3187d66bc0587a56f1b16c7d7b0c0d21741dddba
king <kelvinozak0@gmail.com>
2026-01-28 23:49:55 +0100

    Add pages

--- resources/views/failed.blade.php
+++ resources/views/failed.blade.php
@@ -22,6 +22,7 @@
 </div>
 <footer class="site-footer" id="footer">
 	<a href="{{ url('/') }}">@lang('Go Back to Home')</a>
+	<a href="{{ url('/') }}">@lang('Go Back to Home')</a>
 	<p class="site-footer__fineprint" id="fineprint">@lang('Copyright') @lang('©') {{ date('Y') }} | @lang('All Rights Reserved') <a href="{{ url('/') }}">{{ __(basicControl()->site_title) }}</a></p>
 </footer>
 </body>
