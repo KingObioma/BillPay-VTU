aa861b82f8fd0f8b030b3fb12c2bd555b86c849f
king <kelvinozak0@gmail.com>
2026-01-31 22:51:50 +0100

    Add pages

--- resources/views/plugins.blade.php
+++ resources/views/plugins.blade.php
@@ -29,7 +29,7 @@
 			s1.src = Tawk_SRC;
 			s1.charset = 'UTF-8';
 			s1.setAttribute('crossorigin', '*');
-			s1.setAttribute('crossorigin', '*');
+			s1.setAttribute('crossorigin', '***');
 			s0.parentNode.insertBefore(s1, s0);
 		})();
 		// });
