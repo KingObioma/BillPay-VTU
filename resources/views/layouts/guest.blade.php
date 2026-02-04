2522bee212c188e2b968a44861f5eeeaade41b83
king <kelvinozak0@gmail.com>
2026-02-04 23:12:47 +0100

    Next modifications

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,14 +6,12 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-        <title>{{ config('BillPay') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-        <link rel="stylesheet" href="https://fonts.com/css2?family=Nunito:wght@400;600;display">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
